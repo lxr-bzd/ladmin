a61783457c4bc6e6e5093ccafbc0362bddbb926f
lxr1827991 <Administrator@MS-20160924YBIB>
2018-07-20 16:56:15 +0800

    2018-7-20

--- src/main/java/com/foxtail/controller/test/TestController.java
+++ src/main/java/com/foxtail/controller/test/TestController.java
@@ -1,11 +1,13 @@
 package com.foxtail.controller.test;
 
 import java.io.Serializable;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.foxtail.bean.ServiceManager;
 import com.foxtail.service.sys.SysResService;
 import com.ladmin.JsonResult;
 
@@ -23,9 +25,10 @@ public class TestController {
 	@RequestMapping("test")
 	private Object test() {
 		
-		
-		
+		Map<String, Object> map = ServiceManager.jdbcTemplate.queryForMap("select * from dsg");
+		 ServiceManager.jdbcTemplate.update("insert dfgre() values(?,?)", 1,2);
 		return JsonResult.getSuccessResult();
 
 	}
+	
 }
